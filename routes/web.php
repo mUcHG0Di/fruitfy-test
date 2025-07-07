031a0691424013ea69377bb7379dd2538cb37c4d
Marcos Godoy <marcosanibalgg@gmail.com>
2025-07-07 12:13:09 -0300

    refactor: web routes

--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,20 @@
 
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 Route::get('/', function () {
     return redirect()->route('contacts.index');
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/dashboard', function () {
+        return inertia()->render('Dashboard');
+    })->name('dashboard');
 
     Route::resource('contacts', ContactController::class);
 });
